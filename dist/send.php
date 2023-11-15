f63a4065bed63a18d5fc37cb00281743fb2fa1c5
Evan Rutledge Borden <eborden@frontrowed.com>
2023-11-15 17:12:31 -0600

    Send status codes on failure

--- dist/send.php
+++ dist/send.php
@@ -5,19 +5,21 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
 
   if (!$msg) {
+    http_response_code(400);
     die('Bad msg');
   }
   if (!$email) {
+    http_response_code(400);
     die('Bad email');
   }
   if ($_POST['name']) {
+    http_response_code(400);
     die('Honeypot filled');
   }
 
   $headers = "From: website@evan-borden.com\nReply-To: $email";
   mail("evan@evan-borden.com", "Website Form", wordwrap($msg, 70), $headers);
 
-  header('Location: https://www.evan-borden.com');
+  echo("Sent!");
 }
-
 ?>
