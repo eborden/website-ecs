7690e8725dfaab6279ce5bce2ea0699b6420fbeb
Evan Rutledge Borden <eborden@frontrowed.com>
2023-11-25 20:37:48 -0500

    Vary luminance of hills

--- src/entity/hills.ts
+++ src/entity/hills.ts
@@ -15,8 +15,24 @@ export function init(ECS) {
     position.state.w = 400 - random(0, 20)
     position.state.x = x
     position.state.y = screen.height - position.state.h
-    color.state.hex = randomColor()
+    color.state.hex = colorLuminance(randomColor(), -0.05)
     ECS.addEntity(hill)
   }
 }
 
+function colorLuminance(hex, lum) {
+  // validate hex string
+  hex = String(hex).replace(/[^0-9a-f]/gi, '');
+  if (hex.length < 6) {
+    hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+  }
+  lum = lum || 0;
+  // convert to decimal and change luminosity
+  var rgb = "#", c, i;
+  for (i = 0; i < 3; i++) {
+    c = parseInt(hex.substr(i*2,2), 16);
+    c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+    rgb += ("00"+c).substr(c.length);
+  }
+  return rgb;
+}
