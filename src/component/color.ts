89a780ae7458b526438d24d09faf3b0090f06c3b
Evan Rutledge Borden <eborden@frontrowed.com>
2023-11-26 13:25:12 -0500

    Move luminance to color module

--- src/component/color.ts
+++ src/component/color.ts
@@ -13,3 +13,20 @@ const FALL_COLORS = ['B86821', 'F3D30C', 'E1512C', '507A3A'];
 export const randomColor = (): string => {
   return '#' + FALL_COLORS[random(0, FALL_COLORS.length - 1)]
 }
+
+export function luminance(hex, lum) {
+  // validate hex string
+  hex = String(hex).replace(/[^0-9a-f]/gi, '');
+  if (hex.length < 6) {
+    hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+  }
+  lum = lum || 0;
+  // convert to decimal and change luminosity
+  var rgb = "#", c, i;
+  for (i = 0; i < 3; i++) {
+    c = parseInt(hex.substr(i*2,2), 16);
+    c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+    rgb += ("00"+c).substr(c.length);
+  }
+  return rgb;
+}
--- src/entity/hills.ts
+++ src/entity/hills.ts
@@ -1,7 +1,7 @@
 import random from 'lodash/random'
 import {screen} from '../screen'
 import {randomPositions} from "../component/position"
-import {randomColor} from "../component/color"
+import {randomColor, luminance} from "../component/color"
 
 export function init(ECS) {
   for (var x of randomPositions(Math.round(screen.width / 85))) {
@@ -15,24 +15,7 @@ export function init(ECS) {
     position.state.w = 400 - random(0, 20)
     position.state.x = x
     position.state.y = screen.height - position.state.h
-    color.state.hex = colorLuminance(randomColor(), -0.05)
+    color.state.hex = luminance(randomColor(), -0.05)
     ECS.addEntity(hill)
   }
-}
-
-function colorLuminance(hex, lum) {
-  // validate hex string
-  hex = String(hex).replace(/[^0-9a-f]/gi, '');
-  if (hex.length < 6) {
-    hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
-  }
-  lum = lum || 0;
-  // convert to decimal and change luminosity
-  var rgb = "#", c, i;
-  for (i = 0; i < 3; i++) {
-    c = parseInt(hex.substr(i*2,2), 16);
-    c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-    rgb += ("00"+c).substr(c.length);
-  }
-  return rgb;
 }
